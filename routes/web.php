4c2a32b5f1ea3bd0b56c81ba3ce10bdd14221897
Peter Pike <pikepeter@gmail.com>
2019-05-14 08:46:53 +0800

    after removing testimonials migration and adding spatie/media

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,7 @@
 
 Route::get('/', function () {return view('homepages.welcome');});
 Route::get('/theartist', function () {return view('homepages.theartist');});
-Route::get('/program/how_it_works', function () {return view('homepages.how_it_works');});
-Route::get('/program/nutrition', function () {return view('homepages.nutrition');});
-Route::get('/program/motivation', function () {return view('homepages.motivation');});
-Route::get('/program/community', function () {return view('homepages.community');});
-   Route::Resource('testimonials','TestimonialController');
+
 
 Auth::routes();
 
