86853814f8423a5fbdac15339a011dc8d08baebe
Peter Pike <pikepeter@gmail.com>
2019-06-03 19:52:29 +0800

    added home to pageheader

--- resources/views/layouts/partials/pageheader.blade.php
+++ resources/views/layouts/partials/pageheader.blade.php
@@ -1,6 +1,6 @@
 <div class="bg-gray-200">
     <div class="container mx-auto md:px-0 px-4">
-        <div class="flex text-center md:text-left  py-2  text-xl">
+        <div class="flex justify-between text-center md:text-left  py-2  text-xl">
             @if (\Route::current()->getName() == 'bycategory')
             <div class="-mb-2 pb-2 font-bold  min-w-0 border-b-4 border-pink-600">
                     <p class="text-grey-dark"> Artwork By Category :- {{ $cat->category }}</p>
@@ -9,6 +9,11 @@
             <div class="-mb-2 pb-2 font-bold  min-w-0 border-b-4 border-pink-600">
                     <p class="text-grey-dark">  @yield('title') </p>
             </div>
+            @endif
+            @if (\Route::current()->getName() !== 'root')
+                <div class="mr-2 pb-2 font-bold  min-w-0  ">
+                    <a href="{{ url('/') }}" class="my-2 font-semibold hover:font-bold no-underline"><i class="fas fa-arrow-circle-left"></i> Home</a>
+                </div>
             @endif
         </div>
     </div>
