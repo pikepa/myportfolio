bd3ce81779ea216a10b72a3b84b89c70f8e30e7a
Peter Pike <pikepeter@gmail.com>
2019-06-03 18:52:51 +0800

    changes to formating and adjustment of substr to 100

--- resources/views/dashboard/components/_pricing.blade.php
+++ resources/views/dashboard/components/_pricing.blade.php
@@ -1,10 +1,10 @@
-<div class="font-bold flex">
+<div class="font-bold flex flex-row justify-between ">
             @if ( $product->status === 'For Sale')
                     <div class="mr-1">
                         <h4 class="my-4">{{ $product->status }} :-</h4>
                     </div> 
                     <div>
-                        <h4 class="my-4">RM {{ number_format($product->price/100,2,'.', ',')}}</h4>
+                        <h4 class="my-4  ">RM {{ number_format($product->price/100,2,'.', ',')}}</h4>
                     </div> 
             @elseif ($product->status === 'Sold')
                 <div>
--- resources/views/dashboard/components/dash_main.blade.php
+++ resources/views/dashboard/components/dash_main.blade.php
@@ -2,27 +2,26 @@
  
 @foreach ($products as $product)
     <div class="flex flex-col card  mb-2 " style=" width:325px">
-        <div class=" ">
             <div class="mx-auto text-center">
                  <img class="w-auto rounded-lg w-full" src={{ $product->featured_img }} style="height:325px" alt="Sunset in the mountains">
             </div>
-        </div>
         <div class=" card mt-2 text-center">
             <h1 class="text-xl font-semibold p-2">{{ $product->title }}</h1>
         </div>
-        <div>
-            <p class="flex-1 mt-4">{{ substr($product->description ,0,150) }}  
-            <a class="text-blue-900 font-extrabold no-underline" href="{{ $url = action('ProductController@show', ['id' => $product->id]) }}" >... more <i class="fas fa-angle-double-right"></i></a></p>
-        </div>
-        <div class="flex flex-row justify-between">
+        <div class="flex flex-col justify-between ">
+            <div class="flex-1 h-auto ">
+                <p class=" mt-4">{{ substr($product->description ,0,100) }}  
+                <a class="text-blue-900 font-extrabold no-underline" href="{{ $url = action('ProductController@show', ['id' => $product->id]) }}" >... more <i class="fas fa-angle-double-right"></i></a></p>
+            </div>
 
-            @include('dashboard.components._pricing')
-                        
+            <div class="">
+                @include('dashboard.components._pricing')           
+            </div>
+
+            <div>
+                <p>Published {{ $product->published_date }}</p>
+            </div>
         </div>
-        <div>
-            <p>Published {{ $product->published_date }}</p>
-        </div>
-     
     </div>
 @endforeach
 
