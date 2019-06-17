b4cde12c81f9f7cf07bc7d98b937ceeb9dcbfb3c
Peter Pike <pikepeter@gmail.com>
2019-06-17 20:13:11 +0800

    add message test

---
+++ tests/Feature/ManageMessageTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Message;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ManageMessageTest extends TestCase
+{
+    use WithFaker, RefreshDatabase;
+
+    /** @test */
+    public function a_guest_can_create_a_message()
+    {
+        $response = $this->get('/message/create');
+        $response->assertStatus(200);
+        $response->assertSee('New Message');
+    }
+
+    /** @test */
+    public function a_message_is_added_to_the_database()
+    {
+        $message = factory(Message::class)->create();
+
+        $this->assertDatabaseHas('messages', ['subject' => $message->subject]); 
+        
+    }
+
+    /** @test */
+    public function a_signed_in_user_can_view_messages()
+    {
+        $message = factory(Message::class)->create();
+
+        $this->assertDatabaseHas('messages', ['subject' => $message->subject]); 
+        
+    }
+
+    
+}
