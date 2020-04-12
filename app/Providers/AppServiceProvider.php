14a4bd406294680b6bd9cbad44db80bb87d481a5
Peter <pikepa@users.noreply.github.com>
2020-04-12 18:02:37 +0800

    Apply fixes from StyleCI (#28)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,13 +28,13 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
 
         view()->composer(['dashboard.components.dash_left',
-                           'products.form',
-                           ], function ($view) {
+            'products.form',
+        ], function ($view) {
                                $view->with('categories', Category::where('active', 1)->orderBy('category', 'asc')->get());
                            });
 
         view()->composer(['categories.index',
-                                                ], function ($view) {
+        ], function ($view) {
                                                     $view->with('categories', Category::orderBy('category', 'asc')->get());
                                                 });
     }
