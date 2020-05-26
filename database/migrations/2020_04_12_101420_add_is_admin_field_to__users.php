1bbe25d3b3a0907781f78127f9bcc837dbe8b480
Peter Pike <pikepeter@gmail.com>
2020-05-26 10:01:45 +0800

    Change to migrations

--- database/migrations/2020_04_12_101420_add_is_admin_field_to__users.php
+++ database/migrations/2020_04_12_101420_add_is_admin_field_to__users.php
@@ -26,7 +26,7 @@ class AddIsAdminFieldToUsers extends Migration
     public function down()
     {
         Schema::table('Users', function (Blueprint $table) {
-            //
+            $table->dropColumn('type');
         });
     }
 }
