f720c172108ec7dc089ac801f9c988d09785b79b
Peter Pike <pikepeter@gmail.com>
2020-05-26 10:03:55 +0800

    Update css.blade.php

--- resources/views/layouts/partials/css.blade.php
+++ resources/views/layouts/partials/css.blade.php
@@ -1,4 +1,4 @@
     <!-- Styles -->
-<link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
