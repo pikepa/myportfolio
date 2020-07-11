b93364bc59cbe4b9570ae5b0870dbfa0f9cde9e2
Peter Pike <pikepeter@gmail.com>
2020-07-11 14:43:16 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,12 +6,7 @@
     @include('layouts.partials.meta')
 
     @yield('og_meta')
-    <meta property="og:site_name" content="Hellen Dutch Art.">
-    <meta property="og:title" content="Hellen Dutch Art : Home">
-    <meta property="og:description" content="Contemporary expressionist: Paintings, mixed media & paper Sculptures from Borneo">
-    <meta property="og:image" content="https://hellendutch7.test/images/172">
-    <meta property="og:url" content="https://hellendutch.art">
-    <meta name="twitter:card" content="summary_large_image">
+    
     @yield('addstyles')
 
     @livewireStyles
