468d0b485ca91c14f2317a4e01b67c779bc7e936
Peter Pike <pikepeter@gmail.com>
2020-07-11 15:12:54 +0800

    cleaning up

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -2,11 +2,12 @@
 
 @section('og_meta')
 <meta property="og:site_name" content="Hellen Dutch Art.">
-    <meta property="og:title" content="Hellen Dutch Art : Home">
-    <meta property="og:description" content="Contemporary expressionist: Paintings, mixed media & paper Sculptures from Borneo">
-    <meta property="og:image" content="https://d32wdolnlcperg.cloudfront.net/172/IMG_7029.JPG">
-    <meta property="og:url" content="https://hellendutch.art">
-    <meta name="twitter:card" content="summary_large_image">
+<meta property="og:title" content="Hellen Dutch Art : Home">
+<meta property="og:description" content="Contemporary expressionist: Paintings, mixed media & paper Sculptures from Borneo">
+<meta property="og:image" content="https://d32wdolnlcperg.cloudfront.net/172/IMG_7029.JPG">
+<meta property="og:url" content="https://hellendutch.art">
+<meta name="twitter:card" content="summary_large_image">
+<title>Hellen Dutch Art : Home</title>
 @endsection
 
 @section('title', 'Home')
