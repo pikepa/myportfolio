43611b8d1fc929fee49a7c93f6d47bb941f768f4
Peter Pike <pikepeter@gmail.com>
2020-07-11 14:16:16 +0800

    Minor formatting tweeks

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -1,9 +1,18 @@
 @extends('layouts.app')
 
+
+@section('og_meta')
+<meta property="og:site_name" content="Hellen Dutch Art.">
+<meta property="og:title" content="Hellen Dutch Art : Home">
+<meta property="og:description" content="Contemporary expressionist: Paintings, mixed media & paper Sculptures from Borneo">
+<meta property="og:image" content="https://hellendutch7.test/images/172">
+<meta property="og:url" content="https://hellendutch.art">
+<meta name="twitter:card" content="summary_large_image">
+@endsection
+
 @section('title', 'Home')
 
 @section('content')
-
 @include('layouts.partials.pageheader')
 
 <div class="container mx-auto pb-4">
