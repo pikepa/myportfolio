1c5a51fc54a103e64cbe9113204d8137103bb292
Peter Pike <pikepeter@gmail.com>
2020-07-11 15:51:22 +0800

    Update product_detail.blade.php

--- resources/views/products/product_detail.blade.php
+++ resources/views/products/product_detail.blade.php
@@ -2,10 +2,10 @@
 
 @section('og_meta')
 <meta property="og:site_name" content="Hellen Dutch Art.">
-<meta property="og:title" content="Hellen Dutch Art : Home">
+<meta property="og:title" content="Hellen Dutch Art : {{$product->title}}">
 <meta property="og:description" content="{!! nl2br($product->description) !!}">
 <meta property="og:image" content="{{URL::asset( $product->featured_img)}}">
-<meta property="og:url" content="https://hellendutch.art">
+<meta property="og:url" content="https://hellendutch.art/product/55">
 <meta name="twitter:card" content="summary_large_image">
 <title>Hellen Dutch Art : Home</title>
 @endsection
