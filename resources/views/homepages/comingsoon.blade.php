2b0c0354bc42d2bb1cfb93318f6c7c4b8c21a751
Peter Pike <pikepeter@gmail.com>
2021-04-03 17:50:14 +0800

    formatting changes

--- resources/views/homepages/comingsoon.blade.php
+++ resources/views/homepages/comingsoon.blade.php
@@ -2,24 +2,23 @@
 
     @section('title', 'Why Borneo')
 
-    <x-layout.pageheader />
+        <x-layout.pageheader />
 
-    <div class="container mx-auto pb-4">
+        <div class="container pb-4 mx-auto">
 
-        <div class="flex flex-col md:flex-row justify-between">
+            <div class="flex flex-col justify-between md:flex-row">
 
-            @include('dashboard.components.dash_left')
+                @include('dashboard.components.dash_left')
 
-            <div class="flex flex-col container mx-auto pb-4">
-                <div class="text-center">
-                    <h1 class="font-bold text-3xl m-2 ">New Feature Coming Soon</h1>
+                <div class="container flex flex-col pb-4 mx-auto">
+                    <div class="text-center">
+                        <h1 class="m-2 text-3xl font-bold ">New Feature Coming Soon</h1>
+                    </div>
+                    <div class="mx-auto mb-4 ">
+                        <img class="" src="{{ URL::asset('/images/smiley.jpg') }}">
+                    </div>
                 </div>
-                <div class=" mb-4  mx-auto ">
-                    <img class="  " src="{{URL::asset( '/images/smiley.jpg')}}">
-                </div>
+
             </div>
-
         </div>
-    </div>
-
-</x-layout.app>
+    </x-layout.app>
