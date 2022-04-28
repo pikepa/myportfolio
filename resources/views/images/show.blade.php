a40ef86df9a8c28ac9f1833eb744f59cf64d4865
Peter <pikepa@users.noreply.github.com>
2022-04-28 18:31:27 +0800

    temp mod

--- resources/views/images/show.blade.php
+++ resources/views/images/show.blade.php
@@ -9,6 +9,7 @@
     
     <main class="flex flex-wrap  py-4">
         <div class="w-3/5 mx-auto px-2 py-2">
+        {{$image->geturl('full')}
             <div class="card flex-grow  overflow-hidden">
                 <img class="w-full rounded" src="{{$image->geturl('full')}}" alt="{{$image->name}}">
             </div>
