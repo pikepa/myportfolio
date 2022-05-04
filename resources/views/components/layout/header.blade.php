6598dcd9a45f4ccc4957ebb5ec9275a19330272c
Peter <pikepa@users.noreply.github.com>
2022-05-05 06:51:05 +0800

    returned  sticky in header blade

--- resources/views/components/layout/header.blade.php
+++ resources/views/components/layout/header.blade.php
@@ -1,4 +1,4 @@
-<nav class="fixed w-full bg-green-600 shadow py-2">
+<nav class="sticky w-full bg-green-600 shadow py-2">
     <div class="container mx-auto px-6 md:px-0">
         <div class="font-muli text-blue-darker flex flex-col justify-end items-center"> 
             <h1 class="font-bold text-3xl">{{env('APP_NAME')}}
