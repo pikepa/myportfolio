b476dc5796bae3773c4da1c7e59948619c7a9fa6
Peter <pikepa@users.noreply.github.com>
2022-05-04 14:32:16 +0800

    set up and running using Hellendutch AWS

--- resources/views/components/layout/header.blade.php
+++ resources/views/components/layout/header.blade.php
@@ -1,8 +1,9 @@
-<nav class="sticky w-full bg-gray-400 shadow py-2">
+<nav class="sticky w-full bg-green-600 shadow py-2">
     <div class="container mx-auto px-6 md:px-0">
-        <div class="font-muli text-blue-darker flex flex-col justify-end items-center">
-            <h1 class="font-bold text-3xl">Hellen Dutch Art</h1>
-            <h4 class='font-medium text-lg  text-center'>contemporary expressionist: paintings, mixed media & paper sculptures</h4>
+        <div class="font-muli text-blue-darker flex flex-col justify-end items-center"> 
+            <h1 class="font-bold text-3xl">{{env('APP_NAME')}}
+                @if(env('ARTIST_NAME') !== '') - {{env('ARTIST_NAME')}} @endif</h1>
+            <h4 class='font-medium text-lg  text-center'>{{env('APP_SUB_NAME')}}</h4>
         </div>
     </div>
 </nav>
