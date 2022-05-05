195ddbb7fc24c8a139da0238deecbf0ac31fe759
Peter <pikepa@users.noreply.github.com>
2022-05-05 21:52:23 +0800

    change to og_meta image

--- resources/views/homepages/home.blade.php
+++ resources/views/homepages/home.blade.php
@@ -4,7 +4,7 @@
     <meta property="og:site_name" content="{{env('APP_NAME')}}.">
     <meta property="og:title" content="{{env('APP_NAME')}} : Home">
     <meta property="og:description" content="{{env('APP_SUB_NAME')}}">
-    <meta property="og:image" content="https://d32wdolnlcperg.cloudfront.net/172/IMG_7029.JPG">
+    <meta property="og:image" content="https://d2ue9uje1dz3gg.cloudfront.net/11/UTuwGjqJlkxaFcviaWgPoBmbvsmlVf-metaS2luZ09mVGhlSnVuZ2xlMjAwNS05MDB4Njc1LmpwZWc=-.jpg">
     <meta property="og:url" content="{{env('APP_URL')}}">
     <meta name="twitter:card" content="summary_large_image">
     <title>{{env('APP_NAME')}} : Home</title>
